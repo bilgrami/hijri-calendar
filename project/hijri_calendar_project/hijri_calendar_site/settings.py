190a1861f82178840492c4dc10816605e1a3d2da
Syed A Bilgrami <bilgrami@gmail.com>
2019-06-02 17:01:31 -0700

    removed double quote around STATIC_URL

--- project/hijri_calendar_project/hijri_calendar_site/settings.py
+++ project/hijri_calendar_project/hijri_calendar_site/settings.py
@@ -120,5 +120,4 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-# print ('static url = ', env('STATIC_URL'))
 STATIC_URL = env('STATIC_URL','/static/')
