fc9f2ef97ebef475f4e573b3fef0d1aca859f573
Syed Bilgrami <bilgrami@gmail.com>
2019-06-14 20:50:34 -0700

    added holiday support

--- project/hijri_calendar_project/hijri_calendar_app/admin.py
+++ project/hijri_calendar_project/hijri_calendar_app/admin.py
@@ -1,20 +1,26 @@
 from django.contrib import admin
 
-from .models import DataFile, HijriCalendar
+from .models import (DataFile, HolidayOriginList,
+                     HolidayCountryList, HolidayAliasList,
+                     Holiday, HijriCalendar)
 
 # Register your models here.
 admin.site.register(DataFile)
+admin.site.register(HolidayOriginList)
+admin.site.register(HolidayCountryList)
+admin.site.register(HolidayAliasList)
+admin.site.register(Holiday)
 
 
 @admin.register(HijriCalendar)
 class HijriCalendarAdmin(admin.ModelAdmin):
     list_display = ('date_value',
                     'hijri_date_value',
-                    'day', 'hijri_day',
-                    'month', 'hijri_month',
-                    'year', 'hijri_year')
+                    'day', 'month', 'year',
+                    'hijri_day', 'hijri_month',
+                    'hijri_year', 'is_holiday')
     list_filter = ('data_file', 'date_value', 'year', 'hijri_year',
-                   'hijri_month')
+                   'is_holiday')
     search_fields = ('date_value', 'hijri_date_value',
                      'month_name', 'hijri_month_name')
     date_hierarchy = 'date_value'
@@ -25,6 +31,7 @@ class HijriCalendarAdmin(admin.ModelAdmin):
               ('day', 'month', 'year'),
               'date_value',
               ('hijri_day', 'hijri_month', 'hijri_year'),
-              'hijri_date_value',)
+              'hijri_date_value',
+              'is_holiday', 'holiday_list',)
     save_on_top = True
     # prepopulated_fields = {'hijri_month_name': ('hijri_month',)}
