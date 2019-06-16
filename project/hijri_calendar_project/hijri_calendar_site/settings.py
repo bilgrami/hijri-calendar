396bf547dcc96c55d5f79a9178fb900ff7cf33ae
Syed A Bilgrami <bilgrami@gmail.com>
2019-06-15 23:52:30 -0700

    - changed permission for rest api to be authenticated or readonly - change page size from 10 to 20

--- project/hijri_calendar_project/hijri_calendar_site/settings.py
+++ project/hijri_calendar_project/hijri_calendar_site/settings.py
@@ -52,8 +52,8 @@ INSTALLED_APPS = [
     'django_extensions',
     'rest_framework',
     'django_admin_shell',
+    'material',
     'material.theme.amber',
-    'material'
  ]
 
 MIDDLEWARE = [
@@ -185,11 +185,14 @@ def print_settings():
 # print_settings()
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        ],
     'DEFAULT_PAGINATION_CLASS': (
         'rest_framework.pagination.'
         'PageNumberPagination'
         ),
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 20
 }
 
 # django-admin-shell settings
