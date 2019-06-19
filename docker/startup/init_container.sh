96d41eb942a00a55939df4b3fd4b2b7d5cb6776f
Syed A Bilgrami <bilgrami@gmail.com>
2019-06-19 01:47:14 -0700

    added ProjectName so that we can change PWD to location where manage.py exists

--- docker/startup/init_container.sh
+++ docker/startup/init_container.sh
@@ -7,9 +7,8 @@ echo starting sshd process
 sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config;
 /usr/sbin/sshd
 
-echo "python is located at:"
-which python 
-
-cd $PROJECT_ROOT/hijri_calendar_project/
-echo "Running django server on port $SERVER_PORT"
+cd $PROJECT_ROOT/$PROJECT_NAME/
+echo "Python location: '$(which python)', Python version: '$(python -V)'"
+echo "Current directory '$(pwd)' contains $(ls -1q * | wc -l) files"
+echo "starting django server on port $SERVER_PORT"
 python manage.py runserver 0.0.0.0:$SERVER_PORT
