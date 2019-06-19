1799736ccf87be91ce4c5bf2ec90dd91c1e506c1
Syed Bilgrami <bilgrami@gmail.com>
2019-06-19 14:55:35 -0700

    cache views for 24 hrs

--- project/hijri_calendar_project/hijri_calendar_app/urls.py
+++ project/hijri_calendar_project/hijri_calendar_app/urls.py
@@ -4,11 +4,13 @@ from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name='home'),
-    url(r'^about/$', views.AboutPageView.as_view(), name='about'),
+    url(r'^about/$', 
+        cache_page(24*60*60)(views.AboutPageView.as_view()),
+        name='about'),
     url(r'^holiday/$', 
-        cache_page(60*60)(views.HolidayPageView.as_view()),
+        cache_page(24*60*60)(views.HolidayPageView.as_view()),
         name='holiday'),
     url(r'^calendar_detail/(?P<date_value>\d{4}-\d{2}-\d{2})/$',
-        cache_page(60*60)(views.CalendarDetailPageView.as_view()),
+        cache_page(24*60*60)(views.CalendarDetailPageView.as_view()),
         name='calendar_detail_url'),
 ]
