bd212daf651d639b4073ee5228c9b431a9bf60c0
Syed A Bilgrami <bilgrami@gmail.com>
2019-06-19 02:37:04 -0700

    added redis to count views

--- project/hijri_calendar_project/hijri_calendar_app/views.py
+++ project/hijri_calendar_project/hijri_calendar_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
-from .models import DataFile, HijriCalendar
+from .models import HijriCalendar
 from datetime import date
+import redis
+from django.conf import settings
+# connect to redis
+r = redis.StrictRedis(host=settings.REDIS_HOST,
+                      port=settings.REDIS_PORT,
+                      db=settings.REDIS_DB)
 
 
 class HomePageView(TemplateView):
@@ -19,14 +25,20 @@ class HolidayPageView(TemplateView):
         holiday_calendars = m.filter(date_value__gte=date.today())  \
                              .order_by('date_value')
         data = 'Note: Holidays earlier than today are not displayed'
+        # increment total views by 1
+        total_views = r.incr('calendar-list:views')
         return render(request, 'hijri_calendar_app/holiday.html',
                                {'holiday_calendars': holiday_calendars,
-                                'data': data})
+                                'data': data,
+                                'total_views': total_views})
 
 
 class CalendarDetailPageView(TemplateView):
     def get(self, request, date_value, **kwargs):
         data = get_object_or_404(HijriCalendar,
                                  date_value=date_value)
+        # increment total date views by 1
+        total_views = r.incr('calendar-date:{}:views'.format(str(date_value)))
         return render(request, 'hijri_calendar_app/calendar_detail.html',
-                               {'data': data})
+                               {'data': data,
+                                'total_views': total_views})
