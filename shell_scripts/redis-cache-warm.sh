b8d6b3f93566d7b77b4b354115dae2f3035d3802
Syed A Bilgrami <bilgrami@gmail.com>
2019-06-20 01:13:06 -0700

    script to warm up redis cache

---
+++ shell_scripts/redis-cache-warm.sh
@@ -0,0 +1,35 @@
+echo warming view cache
+curl -s -o nul https://mooncalendar.azurewebsites.net/calendar_detail/2019-06-05/
+curl -s -o nul https://mooncalendar.azurewebsites.net/calendar_detail/2019-08-12/
+curl -s -o nul https://mooncalendar.azurewebsites.net/calendar_detail/2019-08-12/
+curl -s -o nul https://mooncalendar.azurewebsites.net/calendar_detail/2019-09-01/
+curl -s -o nul https://mooncalendar.azurewebsites.net/calendar_detail/2019-09-10/
+curl -s -o nul https://mooncalendar.azurewebsites.net/calendar_detail/2019-11-10/
+curl -s -o nul https://mooncalendar.azurewebsites.net/calendar_detail/2019-12-25/
+curl -s -o nul https://mooncalendar.azurewebsites.net/calendar_detail/2020-03-22/
+curl -s -o nul https://mooncalendar.azurewebsites.net/calendar_detail/2020-04-09/
+curl -s -o nul https://mooncalendar.azurewebsites.net/calendar_detail/2020-04-25/
+curl -s -o nul https://mooncalendar.azurewebsites.net/calendar_detail/2020-05-01/
+curl -s -o nul https://mooncalendar.azurewebsites.net/calendar_detail/2020-05-24/
+curl -s -o nul https://mooncalendar.azurewebsites.net/calendar_detail/2020-07-31/
+curl -s -o nul https://mooncalendar.azurewebsites.net/calendar_detail/2020-08-21/
+curl -s -o nul https://mooncalendar.azurewebsites.net/calendar_detail/2020-08-30/
+curl -s -o nul https://mooncalendar.azurewebsites.net/calendar_detail/2020-10-29/
+curl -s -o nul https://mooncalendar.azurewebsites.net/calendar_detail/2020-12-25/
+echo warming api cache
+curl -s -o nul https://mooncalendar.azurewebsites.net/api/v1/calendar/2019-06-05/
+curl -s -o nul https://mooncalendar.azurewebsites.net/api/v1/calendar/2019-08-12/
+curl -s -o nul https://mooncalendar.azurewebsites.net/api/v1/calendar/2019-09-01/
+curl -s -o nul https://mooncalendar.azurewebsites.net/api/v1/calendar/2019-09-10/
+curl -s -o nul https://mooncalendar.azurewebsites.net/api/v1/calendar/2019-11-10/
+curl -s -o nul https://mooncalendar.azurewebsites.net/api/v1/calendar/2019-12-25/
+curl -s -o nul https://mooncalendar.azurewebsites.net/api/v1/calendar/2020-03-22/
+curl -s -o nul https://mooncalendar.azurewebsites.net/api/v1/calendar/2020-04-09/
+curl -s -o nul https://mooncalendar.azurewebsites.net/api/v1/calendar/2020-04-25/
+curl -s -o nul https://mooncalendar.azurewebsites.net/api/v1/calendar/2020-05-01/
+curl -s -o nul https://mooncalendar.azurewebsites.net/api/v1/calendar/2020-05-24/
+curl -s -o nul https://mooncalendar.azurewebsites.net/api/v1/calendar/2020-07-31/
+curl -s -o nul https://mooncalendar.azurewebsites.net/api/v1/calendar/2020-08-21/
+curl -s -o nul https://mooncalendar.azurewebsites.net/api/v1/calendar/2020-08-30/
+curl -s -o nul https://mooncalendar.azurewebsites.net/api/v1/calendar/2020-10-29/
+curl -s -o nul https://mooncalendar.azurewebsites.net/api/v1/calendar/2020-12-25/
