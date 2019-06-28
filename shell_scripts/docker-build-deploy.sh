69dd4f9f2268911efb66046d71835b86f9cd8799
Syed A Bilgrami <bilgrami@gmail.com>
2019-06-28 01:10:03 -0700

    display product version during product build

--- shell_scripts/docker-build-deploy.sh
+++ shell_scripts/docker-build-deploy.sh
@@ -19,4 +19,8 @@ docker push "$image_full_name";
 echo "Push finished!";
 
 exit 0;
+
+# docker build --rm -f "Dockerfile" -t bilgrami/hijricalendar:latest .
+# docker push bilgrami/hijricalendar:latest
 # docker run --rm -it -p 5000:5000/tcp $image_full_name
+# docker run -it --entrypoint bash -p 5000:5000/tcp bilgrami/hijricalendar:latest
